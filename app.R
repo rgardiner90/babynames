2e908806c214e275c62c67c7af688d89ea4fcd9b
Richard G. Gardiner <rgardiner90@gmail.com>
2019-09-17 14:42:12 -0400

    updated the app for greater clarity
    uses AccessLex staff

--- app.R
+++ app.R
@@ -1,47 +1,132 @@
-library(babynames)
 library(shiny)
+library(babynames)
 library(tidyverse)
-
-possible_names <- c("John", "Lisa", "Chantelle", "Kobi", "Jace",
-                    "Jessica", "Jared", "James", "Calvin", "Richard",
-                    "Julie", "Carter", "Madelyn", "Kyle", "Karley",
-                    "Doug", "Tina", "Zach", "Ethan", "Kelli", "Jack",
-                    "Hannah", "Amelia", "Craig", "Ali", "Eric", "Whitney")
-
-ui <- fluidPage(
-  paste("Comparing Baby Names: Only compare the same gender"),
-  radioButtons(inputId = "gender",
-               label = "Gender",
-               choices = c("F", "M"),
-               selected = "F"),
-  selectInput(inputId = "name",
-              label = "Name",
-              selected = "Julie",
-              choices = possible_names,
-              multiple = TRUE),
-  
-  plotOutput(outputId = "line")
-)
+library(researchr)
 
 
-server <- function(input, output) {
-  output$line <- renderPlot(
-    babynames %>%
-      filter(sex == input$gender,
-             name == str_to_title(input$name)) %>%
-      ggplot(aes(x = year, y = n, color = name)) +
-      geom_line(size = 1) +
-      labs(title = paste0("You Selected: ", input$name),
-           y = "Number of People with that Name",
-           x = "Year") +
-      scale_x_continuous(breaks = c(1880, 1900, 1920, 1940, 1960, 1980, 2000),
-                         labels = c(1880, 1900, 1920, 1940, 1960, 1980, 2000)) +
-      theme_light()
-  )
+accesslex_colors <- c(`main blue` = "#002b49",
+                      `main light blue` = "#006072",
+                      `second yellow` = "#d18a00",
+                      `second orange` = "#d35e13",
+                      `second red` = "#8f1336",
+                      `second maroon` = "#632d4f",
+                      `third green` = "#556221",
+                      `third blue gray` = "#4b4f54",
+                      `third light blue` = "#7e9bc0")
+
+
+al_colors <- function(...) {
+   colors <- c(...)
+   
+   if(is.null(colors))
+      return(accesslex_colors)
+   
+   accesslex_colors[colors]
 }
 
 
+theme_researcher <- function(base_size = 10, font = NA) {
+   txt <- element_text(size = base_size + 2)
+   
+   theme_classic(base_size = base_size, base_family = font) +
+      theme(
+         text = txt,
+         plot.title = element_text(color = al_colors("main blue"), face = "bold"),
+         plot.subtitle = element_text(face = "italic", color = al_colors("main light blue")),
+         axis.title = txt,
+         axis.text = txt,
+         legend.title = txt,
+         legend.text = txt,
+         
+         axis.line.y = element_blank(),
+         axis.line.x = element_blank(),
+         panel.grid.major.y = element_line(color = "gray70", size = 0.1),
+         panel.grid.minor.y = element_line(color = "gray95", size = 0.1),
+         axis.ticks.y = element_blank(),
+         axis.ticks.x = element_blank(),
+         
+         legend.position = "bottom",
+         
+         plot.caption = element_text(hjust = 0)
+      )
+}
 
 
+office_names <- c("Tanya", "Andrew", "Fletcher", "Rachel", "Sara",
+           "Nancy", "Richard", "Aaron", "Tiffane", "Apryl",
+           "Ana", "Rob", "Ray", "Aisha", "Anna", "Lauren",
+           "Matthew", "Isaac")
+
+data(babynames)
+
+babynames2 <- babynames %>%
+   mutate(name = name) %>%
+   filter(name %in% office_names) %>%
+   mutate(sex = ifelse(sex == "F", "Female", "Male"))
+
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+   
+   # Application title
+   titlePanel("Baby Names"),
+   
+   # Sidebar with a slider input for number of bins 
+   sidebarLayout(
+     sidebarPanel(
+       selectInput(inputId = "name",
+                 label = "Select Name:",
+                 choices = office_names,
+                 selected = "Richard",
+                 selectize = TRUE,
+                 multiple = TRUE),
+       helpText("It is recommended to choose individuals of the same gender (You will have trouble finding very many",
+                "women named Richard)."),
+       radioButtons(inputId = "sex",
+                  label = "Select Gender",
+                  choices = c("Male", "Female"),
+                  selected = "Male"),
+       sliderInput(inputId = "cutoff",
+                   label = "Would you like to filter Birth Year?",
+                   min = 1880,
+                   max = 2017,
+                   value = c(1880, 2017),
+                   sep = "")
+      ),
+      
+      # Show a plot of the generated distribution
+      mainPanel(
+         plotOutput(outputId = "linePlot")
+      )
+   )
+)
+
+
+
+server <- function(input, output) ({
+   
+   output$linePlot <- renderPlot({
+      
+      
+      req(input$name)
+      
+      babynames2 %>%
+         filter(name == input$name,
+                sex == input$sex,
+                year > input$cutoff[1],
+                year < input$cutoff[2]) %>%
+         ggplot(aes(x = year, y = n, color = name, group = name)) +
+         geom_line(lwd = 1.5) +
+         theme_researcher() +
+         researchr::scale_color_al() +
+         labs(x = "Birth Year", y = "Number of Social Security Cards Issued",
+              color = "Name:")
+      
+      
+   })
+})
+
+# Run the application 
 shinyApp(ui = ui, server = server)
 
